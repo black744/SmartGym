0bea9f5c81aaaca25574e74367d22675a9ea0edb
ulises <atganarazulises@gmail.com>
2022-10-21 17:58:23 -0300

    boton preba

--- carpetaprueba.html/app.js
+++ carpetaprueba.html/app.js
@@ -1,25 +1,19 @@
-document.querySelector(".addParam").addEventListener("click",addParam);
-document.querySelector(".showResults").addEventListener("click",showResults);
+$("#success").click(function () {
+  $(".notify").toggleClass("active");
+  $("#notifyType").toggleClass("success");
+  
+  setTimeout(function(){
+    $(".notify").removeClass("active");
+    $("#notifyType").removeClass("success");
+  },2000);
+});
 
-var parametros = [];
-var valores = [];
-
-function addParam(){
-    let html = document.querySelector(".container").innerHTML;
-    let newHTML = '<div><input type="text" class="parametro" placeholder="parametro"><input type="number" class="valor" placeholder="valor"></div>'; 
-    document.querySelector(".container").innerHTML = html + newHTML;
-} 
-
-
-function showResults(){
-    for (var i = 0; i < document.querySelectorAll('.parametro').length ; i++) {
-      parametros.push(document.querySelectorAll('.parametro')[i].value);
-      valores.push(parseInt(document.querySelectorAll(".valor")[i].value));
-    }
-    var data = [{
-      x: parametros,
-      y: valores,
-      type: "linear"
-    }];
-    Plotly.newPlot("grafico",data);
-}
+$("#failure").click(function () {
+  $(".notify").addClass("active");
+  $("#notifyType").addClass("failure");
+  
+  setTimeout(function(){
+    $(".notify").removeClass("active");
+    $("#notifyType").removeClass("failure");
+  },2000);
+});
