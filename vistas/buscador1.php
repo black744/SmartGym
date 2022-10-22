de40f8bb7571cc3cc27541080d6d0c1e22807be0
black744 <56609173+black744@users.noreply.github.com>
2022-10-21 22:23:12 -0300

    ACT BUSCADOR

--- vistas/buscador1.php
+++ vistas/buscador1.php
@@ -19,6 +19,9 @@ $image = $row['image'];
 <style>
 .s-p{
 position: relative;
+z-index:1;
+top: 68vh;
+left: 28vw;
 margin-left: 3%;
 height: 80px;
 width: 470px;
@@ -26,6 +29,23 @@ margin-top:2vh;
 font-size: 10px;
 }
 </style>
+
+<script>
+    const selec = document.querySelectorAll(".se")
+    const testo = document.querySelectorAll(".s-p")
+    console.log(btn);
+    console.log(conteinerr);
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener("click", function() {
+            if (conteinerr[i].style.display === "block") {
+                conteinerr[i].style.display = "none"
+            } else {
+                conteinerr[i].style.display = "block"
+            }
+        });
+    }
+    </script>
+
 <?php
 $buscador = mysqli_query($conex, "SELECT * FROM ejercicios WHERE id_ejercicio LIKE LOWER('%" . $_POST["s-e"] . "%')");
 $resultado = mysqli_fetch_assoc($buscador)
