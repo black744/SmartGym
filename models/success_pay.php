0cc3a1ae61a103b52815fb953eed6215ae2c7637
Fabricio <74994692+ZicodeliK@users.noreply.github.com>
2022-11-01 01:10:09 -0300

    act 1ro 11

--- models/success_pay.php
+++ models/success_pay.php
@@ -4,8 +4,10 @@ $conex = conectar();
 session_start();
 
 $idusuario_pago_aceptado=$_GET["idusuario"];
+$tipo_suscripcion=$_GET['plantype_set'];
 
-$act_pago="UPDATE datos SET estado_cuenta='1', ultimopago= NOW() WHERE idusuario='$idusuario_pago_aceptado'";
+
+$act_pago="UPDATE datos SET estado_cuenta='1', plantype='$tipo_suscripcion', ultimopago= NOW() WHERE idusuario='$idusuario_pago_aceptado'";
 $query_pago_aceptado=mysqli_query($conex, ($act_pago));
 
 echo("<script>alert('Pago acreditado correctamente, ya puede usar el servicio');</script>");
